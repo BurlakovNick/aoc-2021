9cf7f1e9db08341b4cdae3e00ccf950a8c045fe2
Nick Burlakov <burlakov.nick@gmail.com>
2021-12-08 19:21:59 +0500

    fix

--- src/aoc_2021/day08.clj
+++ src/aoc_2021/day08.clj
@@ -2,8 +2,6 @@
   (:require [aoc-2021.core :refer [sum slurp-strings parse-ints sum abs]])
   (:require [clojure.math.combinatorics :refer [permutations]]))
 
-(slurp-strings "day08.txt")
-
 (defn parse-line [str]
   (as-> (clojure.string/split str #" ") $
         (split-with #(not= "|" %) $)
@@ -20,26 +18,19 @@
 "Hard"
 (def digit-codes {"abcefg" \0, "cf" \1, "acdeg" \2, "acdfg" \3, "bcdf" \4, "abdfg" \5, "abdefg" \6, "acf" \7, "abcdefg" \8, "abcdfg" \9})
 
-(defn get-digit
-  ([code]
-   (get digit-codes (apply str (sort code))))
-  ([code mapping]
-   (get-digit (apply str (mapv mapping code)))))
-
 (defn get-char [i] (char (+ (int \a) i)))
 (defn to-mapping [perm] (into {} (map-indexed (fn [i, ch] [ch (get-char i)]) perm)))
 
-(defn is-valid? [perm digits]
-  (let [mapping (to-mapping perm)]
-    (every? #(some? (get-digit % mapping)) digits)))
+(defn get-digit
+  ([code] (get digit-codes (apply str (sort code))))
+  ([code perm] (get-digit (mapv (to-mapping perm) code))))
 
-(defn decipher [digits]
-  (let [perms (permutations "abcdefg")]
-    (first (filter #(is-valid? % digits) perms))))
+(defn is-valid? [perm digits] (every? #(some? (get-digit % perm)) digits))
 
 (defn solve [digits tests]
-  (let [mapping (to-mapping (decipher digits))
-        solved (mapv #(get-digit % mapping) tests)]
+  (let [perms (permutations "abcdefg")
+        valid-perm (first (filter #(is-valid? % digits) perms))
+        solved (mapv #(get-digit % valid-perm) tests)]
     (apply str solved)))
 
 (sum (mapv #(Integer/parseInt (apply solve %)) (parse)))
