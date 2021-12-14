7289b65709adb6297c1f2aa24046ea331784b641
Nick Burlakov <burlakov.nick@gmail.com>
2021-12-14 11:20:41 +0500

    Fix

--- src/aoc_2021/day14.clj
+++ src/aoc_2021/day14.clj
@@ -14,18 +14,22 @@
 (def insertions (second input))
 
 (defn zip [line] (frequencies (into [] (partition 2 1 line))))
-(defn += [k v freq] (update freq k (fnil + 0) v))
+(defn += [freq k v] (update freq k (fnil + 0) v))
+(defn ++= [freq v keys] (reduce (fn [res k] (+= res k v)) freq keys))
 
 (defn insert [freq]
   (reduce (fn [result [[left right] cnt]]
             (let [mid (get insertions [left right])]
-              (+= [mid right] cnt (+= [left mid] cnt result))))
+              (++= result cnt [[mid right] [left mid]])))
           {} freq))
 
 (defn unzip-counts [freq]
-  (let [counts (reduce (fn [res [[a b] cnt]] (+= b cnt (+= a cnt res))) {} freq)
-        fixed (+= (last start) 1 (+= (first start) 1 counts))]
+  (let [counts (reduce
+                 (fn [res [[a b] cnt]] (++= res cnt [a b]))
+                 {} freq)
+        fixed (++= counts 1 [(first start) (last start)])]
     (into {} (mapv (fn [[k v]] [k (quot v 2)]) fixed))))
+
 
 (let [zipped (zip start)
       result (nth (iterate insert zipped) 40)
