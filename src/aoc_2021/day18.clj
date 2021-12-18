c65da457d5b373f5a89185d7c9dc2895004331d3
Nick Burlakov <burlakov.nick@gmail.com>
2021-12-18 19:55:14 +0500

    Refactoring

--- src/aoc_2021/day18.clj
+++ src/aoc_2021/day18.clj
@@ -1,49 +1,36 @@
-(ns aoc-2021.day18
-  (:require [aoc-2021.core :refer [sum]]))
+(ns aoc-2021.day18)
 
 (defn parse [] (load-file "resources/day18.txt"))
 
 (defn find-leaf
-  ([v pred] (find-leaf v pred 0 0))
+  ([v pred] (first (find-leaf v pred 0 0)))
   ([v pred pos depth]
    (if (integer? v)
-     (if (pred v pos depth) {:found v :pos pos :depth depth} {:next-pos (inc pos)})
-     (loop [i 0 pos pos]
-       (if (= i (count v))
-         {:next-pos pos}
-         (let [item (nth v i)
-               result (find-leaf item pred pos (inc depth))
-               pos (:next-pos result)]
-           (if (contains? result :found)
-             result
-             (recur (inc i) pos))))))))
+     (if (pred v pos depth)
+       [{:found v :pos pos :depth depth} (inc pos)]
+       [nil (inc pos)])
+     (let [[left right] v
+           [found pos] (find-leaf left pred pos (inc depth))]
+       (if (some? found)
+         [found pos]
+         (find-leaf right pred pos (inc depth)))))))
 
 (defn replace-subtree
-  ([pred new-val v] (:result (replace-subtree pred new-val 0 v)))
+  ([pred new-val v] (first (replace-subtree pred new-val 0 v)))
   ([pred new-val pos v]
    (if (integer? v)
-     {:result   (if (pred pos pos) new-val v)
-      :next-pos (inc pos)}
-     (let [left pos]
-       (loop [i 0 pos pos result []]
-         (if (= i (count v))
-           {:result   (if (pred left pos) new-val result)
-            :next-pos pos}
-           (let [item (nth v i)
-                 child (replace-subtree pred new-val pos item)
-                 pos (:next-pos child)]
-             (recur (inc i) pos (conj result (:result child))))))))))
+     [(if (pred pos pos) new-val v) (inc pos)]
+     (let [from-pos pos
+           [left right] v
+           [left-result pos] (replace-subtree pred new-val pos left)
+           [right-result pos] (replace-subtree pred new-val pos right)]
+       (if (pred from-pos pos)
+         [new-val pos]
+         [[left-result right-result] pos])))))
 
-(defn find-nth-leaf [v n]
-  (:found (find-leaf v (fn [v pos depth] (= pos n))) 0))
-
-(defn find-explodes [v]
-  (let [found (find-leaf v (fn [v pos depth] (>= depth 5)))]
-    (if (contains? found :found) found nil)))
-
-(defn find-split [v]
-  (let [found (find-leaf v (fn [v pos depth] (>= v 10)))]
-    (if (contains? found :found) found nil)))
+(defn nth-value [v n] (:found (find-leaf v (fn [v pos depth] (= pos n))) 0))
+(defn find-explodes [v] (find-leaf v (fn [v pos depth] (>= depth 5))))
+(defn find-split [v] (find-leaf v (fn [v pos depth] (>= v 10))))
 
 (defn replace-nth-leaf [n new-val v]
   (replace-subtree (fn [l r] (and (= l n) (= r n))) new-val v))
@@ -51,16 +38,13 @@
 (defn replace-explosion [n new-val v]
   (replace-subtree (fn [l r] (and (= l n) (= r (+ 2 n)))) new-val v))
 
-(defn explodes [v]
+(defn explode [v]
   (let [exploding (find-explodes v)]
-    (if (not exploding)
-      v
+    (if (not exploding) v
       (let [prev-pos (dec (:pos exploding))
             next-pos (+ 2 (:pos exploding))
-            left-explode-val (find-nth-leaf v (:pos exploding))
-            right-explode-val (find-nth-leaf v (inc (:pos exploding)))
-            prev-val (+ left-explode-val (find-nth-leaf v prev-pos))
-            next-val (+ right-explode-val (find-nth-leaf v next-pos))]
+            prev-val (+ (nth-value v (:pos exploding)) (nth-value v prev-pos))
+            next-val (+ (nth-value v (inc (:pos exploding))) (nth-value v next-pos))]
         (->> v
              (replace-nth-leaf prev-pos prev-val)
              (replace-nth-leaf next-pos next-val)
@@ -68,40 +52,33 @@
 
 (defn split [v]
   (let [splitting (find-split v)]
-    (if (not splitting)
-      v
-      (let [pos (:pos splitting)
-            val (:found splitting)
+    (if (not splitting) v
+      (let [val (:found splitting)
             left (quot val 2)
             right (+ left (mod val 2))]
-        (replace-nth-leaf pos [left right] v)))))
+        (replace-nth-leaf (:pos splitting) [left right] v)))))
 
 (defn reduce-number [number]
-  (let [number' (explodes number)]
-    (if (not= number' number)
-      (reduce-number number')
-      (let [number' (split number)]
-        (if (not= number' number)
-          (reduce-number number')
-          number')))))
+  (let [exploded (explode number) splitted (split number)]
+    (cond
+      (not= exploded number) (reduce-number exploded)
+      (not= splitted number) (reduce-number splitted)
+      :else number)))
 
-(defn add [left right] [left right])
-
-(defn sum-of [numbers]
-  (reduce (fn [l r] (reduce-number (add l r))) (first numbers) (drop 1 numbers)))
+(defn sum [numbers]
+  (reduce (fn [l r] (reduce-number [l r])) (first numbers) (drop 1 numbers)))
 
 (defn magnitude [v]
   (if (integer? v)
     v
-    (let [[l r] v]
-      (+ (* 3(magnitude l)) (* 2 (magnitude r))))))
+    (+ (* 3 (magnitude (first v))) (* 2 (magnitude (second v))))))
 
 "Easy"
-(magnitude (sum-of (parse)))
+(magnitude (sum (parse)))
 
 "Hard"
 (let [numbers (parse)
       n (count numbers)
       pairs (for [i (range 0 n) j (range 0 n) :when (not= i j)]
               [(nth numbers i) (nth numbers j)])]
-  (apply max (map #(magnitude (sum-of %)) pairs)))
+  (apply max (map #(magnitude (sum %)) pairs)))
