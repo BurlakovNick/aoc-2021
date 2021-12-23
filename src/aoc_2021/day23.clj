03bfa15e0ed6288bb142d813b03d373dcaecc5e1
Nick Burlakov <burlakov.nick@gmail.com>
2021-12-23 16:02:55 +0500

    tmp

--- src/aoc_2021/day23.clj
+++ src/aoc_2021/day23.clj
@@ -22,49 +22,65 @@
            \C 7
            \D 9))))
 
-(defn hallway? [[x y]] (= x 1))
+(defn hallway? [[x y]]
+  (and (= x 1) (contains? #{1 2 4 6 8 10 11} y)))
+
 (def hallway (into [] (for [y [1 2 4 6 8 10 11]] [1 y])))
-(def all-chambers (into [] (for [x [2 3] y [3 5 7 9]] [x y])))
-(defn final-chamber [amphipod] (filterv #(final-chamber? amphipod %) all-chambers))
+(defn all-chambers [grid] (into [] (for [x (irange 2 (- (count grid) 2)) y [3 5 7 9]] [x y])))
+(defn final-chamber [grid amphipod] (filterv #(final-chamber? amphipod %) (all-chambers grid)))
 
 (defn allowed-final-chamber [grid amphipod]
-  (let [finals (final-chamber amphipod)]
-    (if (every? #(contains? #{\. amphipod} (get-in grid %)) finals)
+  (let [finals (final-chamber grid amphipod)]
+    (if (every? #(contains? #{\. amphipod \#} (get-in grid %)) finals)
       finals
       [])))
 
 (defn amphipods [grid]
-  (for [x (irange 1 3) y (irange 1 11)
+  (for [x (irange 1 (dec (count grid))) y (irange 1 11)
         :let [cell (get-in grid [x y] \#)]
         :when (amphipod? cell)]
     [x y]))
 
-(defn dfs [grid from to visited len]
-  (cond
-    (and (= from to)) len
-    (contains? visited from) nil
-    :else
-    (let [neighbors (filter #(= \. (get-in grid %)) (grid-neighbors grid from))
-          visited' (conj visited from)]
-      (first (filterv #(some? %) (mapv #(dfs grid % to visited' (inc len)) neighbors))))))
+(defn mark-final-amphipods [grid]
+  (reduce
+    (fn [grid [amphi [x y]]]
+      (if (and
+            (= (get-in grid [x y]) amphi)
+            (= (get-in grid [(inc x) y]) \#))
+        (modify grid [x y] \#)
+        grid))
+    grid (for [amphi [\A \B \C \D]
+               p (reverse (final-chamber grid amphi))]
+           [amphi p])))
 
-(defn can-move [grid from to] (some? (dfs grid from to #{} 0)))
+(defn dfs [grid from visited len]
+  (if
+    (contains? visited from) visited
+    (let [neighbors (filter #(= \. (get-in grid %)) (grid-neighbors grid from))
+          visited' (assoc visited from len)]
+      (reduce (fn [visited' to]
+                (dfs grid to visited' (inc len)))
+              visited' neighbors))))
+
+(defn reachable [grid from] (dfs grid from {} 0))
 
 (def amphi-cost {\A 1 \B 10 \C 100 \D 1000})
-(defn move-cost [grid from to]
-  (let [len (dfs grid from to #{} 0)
-        amphi (get-in grid from)]
+(defn move-cost [grid from len]
+  (let [amphi (get-in grid from)]
     (* len (get amphi-cost amphi))))
 
 (defn allowed-to-move [grid [x y]]
-  (let [amphipod (get-in grid [x y])]
+  (let [grid (mark-final-amphipods grid)
+        amphipod (get-in grid [x y])]
     (cond
       (not (amphipod? amphipod)) []
-      (chamber? [x y]) (->> (concat hallway (allowed-final-chamber grid amphipod))
-                            (remove #(= [1 y] %))
-                            (filterv #(can-move grid [x y] %)))
-      (hallway? [x y]) (->> (allowed-final-chamber grid amphipod)
-                            (filterv #(can-move grid [x y] %))))))
+      (chamber? [x y]) (->> (reachable grid [x y])
+                            (filter (fn [[p _]] (hallway? p))))
+      (hallway? [x y]) (->> (reachable grid [x y])
+                            (filter (fn [[[nx ny] _]]
+                                      (and
+                                        (final-chamber? amphipod [nx ny])
+                                        (= \# (get-in grid [(inc nx) ny])))))))))
 
 (defn update-min [[cost queue] k v]
   (if (not (contains? cost k))
@@ -73,33 +89,53 @@
       [cost queue]
       [(assoc cost k v) (conj (disj queue [(cost k) k]) [v k])])))
 
-(def final-state ["#############" "#...........#" "###A#B#C#D###" "  #A#B#C#D#" "  #########"])
-
 (defn move [grid from to]
   (let [amphi (get-in grid from)]
     (modify (modify grid from \.) to amphi)))
 
-(defn find-min-path [grid]
+(defn find-min-path [grid final]
   (loop [cost {grid 0} queue (sorted-set [0 grid]) i 0]
-    (if (= i 1000000)
-      -100
-      (let [[[cur-cost grid] & _] queue]
-        (pprint grid)
-        (println cur-cost)
-        (cond
-          (not (some? grid)) (cost final-state)
-          (= grid final-state) (cost final-state)
-          :else (let [new-states (for [amphi (amphipods grid)
-                                       to (allowed-to-move grid amphi)
-                                       :let [new-cost (+ cur-cost (move-cost grid amphi to))
-                                             new-grid (move grid amphi to)]]
-                                   [new-grid new-cost])
-                      [cost' queue'] (reduce
-                                       (fn [res [new-grid new-cost]]
-                                         (update-min res new-grid new-cost))
-                                       [cost queue] new-states)]
-                  (recur cost' (disj queue' [(cost grid) grid]) (inc i))))))))
+    (let [[[cur-cost grid] & _] queue]
+      (if (= 0 (mod i 1000))
+        (println cur-cost))
+      (cond
+        (not (some? grid)) (cost final)
+        (= grid final) (cost final)
+        :else (let [new-states (for [amphi (amphipods grid)
+                                     [to len] (allowed-to-move grid amphi)
+                                     :let [new-cost (+ cur-cost (move-cost grid amphi len))
+                                           new-grid (move grid amphi to)]]
+                                 [new-grid new-cost])
+                    [cost' queue'] (reduce
+                                     (fn [res [new-grid new-cost]]
+                                       (update-min res new-grid new-cost))
+                                     [cost queue] new-states)]
+                (recur cost' (disj queue' [(cost grid) grid]) (inc i)))))))
 
-(find-min-path (parse))
-(pprint (modify (parse) [1 1] \E))
-(pprint (parse))
+(time (find-min-path ["#############"
+                      "#...........#"
+                      "###B#C#B#D###"
+                      "  #A#D#C#A#"
+                      "  #########"]
+
+                     ["#############"
+                      "#...........#"
+                      "###A#B#C#D###"
+                      "  #A#B#C#D#"
+                      "  #########"]))
+
+(time (find-min-path (parse)
+                     ["#############"
+                      "#...........#"
+                      "###A#B#C#D###"
+                      "  #A#B#C#D#"
+                      "  #########"]))
+
+(time (find-min-path (into [] (concat (take 3 (parse)) ["  #D#C#B#A#" "  #D#B#A#C#"] (drop 3 (parse))))
+                     ["#############"
+                      "#...........#"
+                      "###A#B#C#D###"
+                      "  #A#B#C#D#"
+                      "  #A#B#C#D#"
+                      "  #A#B#C#D#"
+                      "  #########"]))
